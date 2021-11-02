ed132a2293040e92d3ae81832efe4c4d690a9955
chengqionghua <1169121469@qq.com>
2021-11-03 01:47:21 +0800

    deploy

---
+++ precache-manifest.7a457fab46835f3f1bfe3d31608c6aa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f91ccc2115c59300a9",
+    "url": "/npc.new_morney_website/css/app.9e29dc1d.css"
+  },
+  {
+    "revision": "449216411932d4817538faab1b7d9cf7",
+    "url": "/npc.new_morney_website/index.html"
+  },
+  {
+    "revision": "98f91ccc2115c59300a9",
+    "url": "/npc.new_morney_website/js/app.a01831fb.js"
+  },
+  {
+    "revision": "164ee477448b57736f88",
+    "url": "/npc.new_morney_website/js/chunk-vendors.600a3144.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/npc.new_morney_website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/npc.new_morney_website/robots.txt"
+  }
+]);
